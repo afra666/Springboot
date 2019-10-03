5e6c4c44cdb60de854a6533d0f00ae7e0414db0a
afra666 <48674984+afra666@users.noreply.github.com>
2019-10-03 20:43:05 +0800

    Update 006.个文件代码.java

--- 006.个文件代码.java
+++ 006.个文件代码.java
@@ -37,7 +37,28 @@ public class PersonController {
         return person;    
     }    
 }    
-****************************************************4.测试页面  
+****************************************************4.HelloController.java 
+package p.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+//@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    //@RequestMapping
+
+
+    @Override
+    public String toString() {
+        return "This is from web info";
+    }
+}
+****************************************************5.测试页面  
 http://localhost:8080/person    
 
 
