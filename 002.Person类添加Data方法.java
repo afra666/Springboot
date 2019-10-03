6c2a464468508285359d3d995a6b0d5e8821204e
afra666 <48674984+afra666@users.noreply.github.com>
2019-10-03 18:15:48 +0800

    Create 002.Person类添加Data方法.java

---
+++ 002.Person类添加Data方法.java
@@ -0,0 +1,9 @@
+1.从IDEA商店下载lomlok，（需要显示全部）
+2.pom添加
+<dependency>
+            <groupId>org.projectlombok</groupId>
+            <artifactId>lombok</artifactId>
+            <optional>true</optional>
+</dependency>
+3.添加@Data
+4.双击@Data---->import class
