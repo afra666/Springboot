291cff2e496e4a2b0ad48ca636962fbdc4e3009a
afra666 <48674984+afra666@users.noreply.github.com>
2019-11-25 17:57:33 +0800

    Create 034.shell-hello并保存.java

---
+++ 034.shell-hello并保存.java
@@ -0,0 +1,25 @@
+//springboot web-ubuntu
+package p;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.*;
+
+@SpringBootApplication
+public class A2Application {
+
+    public static void main(String[] args) throws IOException {
+        SpringApplication.run(A2Application.class, args);
+
+        Process p = Runtime.getRuntime().exec(new String[]{
+                "bash", "-c",
+                "echo Hello World > ./output"
+        });
+    }
+
+}
+
+/***
+
+ */
